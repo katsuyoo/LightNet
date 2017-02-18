724b1470f3d0d1bdc508f5a21ff3e5875868ea09
yechengxi <Chengxi Ye>
2017-02-17 22:17:35 -0500

    20170217_nntoolbox

--- CoreModules/layers/bnorm.m
+++ CoreModules/layers/bnorm.m
@@ -37,36 +37,31 @@ function [ net,y,dzdw,dzdb,opts ] = bnorm( net,x,layer_idx,dzdy,opts )
     if ~isfield(net.layers{1,layer_idx},'weights')        
         sz=[shape_x(end-1),1];
         net.layers{1,layer_idx}.weights{1}=ones(sz,'like',x);
-        for i=2:4
-            net.layers{1,layer_idx}.weights{i}=zeros(sz,'like',x);
-        end
+        net.layers{1,layer_idx}.weights{2}=zeros(sz,'like',x);
         for i=1:2
             net.layers{1,layer_idx}.momentum{i}=zeros(sz,'like',x);
         end        
     end
     
-    if net.iterations_bn==0
+    if (net.iterations_bn==0||opts.reset_mom==1||length(net.layers{1,layer_idx}.weights)<4) && isempty(dzdy)
         for i=3:4
             net.layers{1,layer_idx}.weights{i}=0;
         end
-    end
+    end    
     mom_factor=1-opts.parameters.mom_bn.^(net.iterations_bn+1);
-    
     
     if(opts.training&&isempty(dzdy))
         net.layers{1,layer_idx}.weights{3}=opts.parameters.mom_bn*net.layers{1,layer_idx}.weights{3}+(1-opts.parameters.mom_bn)*mean(x,2);   
         net.layers{1,layer_idx}.weights{4}=opts.parameters.mom_bn*net.layers{1,layer_idx}.weights{4}+(1-opts.parameters.mom_bn)*mean(x.^2,2);  
-
     end
-            
+    
+    mu=net.layers{1,layer_idx}.weights{3}./mom_factor;
+    sigma=(net.layers{1,layer_idx}.weights{4}./mom_factor+opts.parameters.eps_bn).^0.5;
+    
     if(isempty(dzdy))
-        
-        net.layers{layer_idx}.x_n=bsxfun(@minus,x,net.layers{1,layer_idx}.weights{3}./mom_factor);
-        net.layers{layer_idx}.x_n=bsxfun(@rdivide,net.layers{layer_idx}.x_n,(net.layers{1,layer_idx}.weights{4}./mom_factor+opts.parameters.eps_bn).^0.5);
-        
-        y=bsxfun(@times,net.layers{layer_idx}.x_n,net.layers{1,layer_idx}.weights{1});
+        x=bsxfun(@minus,x,mu);
+        y=bsxfun(@times,x,net.layers{1,layer_idx}.weights{1}./sigma);
         y=bsxfun(@plus,y,net.layers{1,layer_idx}.weights{2});
-        
         
     else
         
@@ -77,24 +72,22 @@ function [ net,y,dzdw,dzdb,opts ] = bnorm( net,x,layer_idx,dzdy,opts )
             dzdy=permute(dzdy,[2,1,3]);dzdy=reshape(dzdy,size(dzdy,1),[]);
         end
         
-        dzdw=mean(dzdy.*net.layers{layer_idx}.x_n,2);
-        dzdb=mean(dzdy,2);
-        
+        x=bsxfun(@minus,x,mu);
+        x=bsxfun(@rdivide,x,sigma);            
+        dzdw=sum(dzdy.*x,2)./shape_x(end);
+        dzdb=sum(dzdy,2)./shape_x(end);
+        denom=size(x,2)./shape_x(end);
         if ~opts.parameters.simple_bn
-            %the complicated version         
-            tmp=bsxfun(@minus,x,net.layers{1,layer_idx}.weights{3}./mom_factor);
-            tmp=bsxfun(@rdivide,tmp,(net.layers{1,layer_idx}.weights{4}./mom_factor+opts.parameters.eps_bn).^(0.5));
-            tmp=bsxfun(@times,dzdw,tmp);
-            tmp=bsxfun(@plus,dzdb,tmp);
+            %the complicated version
+            tmp=bsxfun(@times,dzdw./denom,x);
+            tmp=bsxfun(@plus,dzdb./denom,tmp);
             dzdy=(dzdy-tmp.*(1-opts.parameters.mom_bn));
             clear tmp;
             %[max(abs(dzdy(:))), max(abs(tmp(:)))]
         end
         
         %the simple version:
-        y=bsxfun(@times,dzdy,net.layers{1,layer_idx}.weights{1});
-        y=bsxfun(@rdivide,y,(net.layers{1,layer_idx}.weights{4}./mom_factor+opts.parameters.eps_bn).^(0.5));
-
+        y=bsxfun(@times,dzdy,net.layers{1,layer_idx}.weights{1}./sigma);
     end
     
     if batch_dim==4
@@ -104,6 +97,5 @@ function [ net,y,dzdw,dzdb,opts ] = bnorm( net,x,layer_idx,dzdy,opts )
     if batch_dim==3 
         y=reshape(y,shape_x(2),shape_x(1),shape_x(3));
         y=permute(y,[2,1,3]);
-    end
-        
+    end        
 end
